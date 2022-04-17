1eba96fc0ef780174b03d79063856659ef5a559b
pachipico <pachipico1@gmail.com>
2022-04-17 23:31:50 +0900

    2022.04.17

---
+++ 2022.04.17.java
@@ -0,0 +1,57 @@
+// Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
+
+// If you want to know more: http://en.wikipedia.org/wiki/DNA
+
+// In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". You function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+// More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
+
+// Example: (input --> output)
+
+// "ATTGC" --> "TAACG"
+// "GTAT" --> "CATA"
+// dnaStrand []        `shouldBe` []
+// dnaStrand [A,T,G,C] `shouldBe` [T,A,C,G]
+// dnaStrand [G,T,A,T] `shouldBe` [C,A,T,A]
+// dnaStrand [A,A,A,A] `shouldBe` [T,T,T,T]
+
+// 내 답안
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
+
+public class DnaStrand {
+  public static String makeComplement(String dna) {
+    //Your code
+	  return Stream.of(dna.split("")).map(v -> {
+			if(v.equals("A")) return "T";
+			if(v.equals("T")) return "A";
+			if(v.equals("C")) return "G";
+			if(v.equals("G")) return "C";
+			else return "d";
+		}).collect(Collectors.joining(""));
+  }
+}
+
+// 모범 답안
+public class DnaStrand {
+  public static String makeComplement(String dna) {
+    char[] chars = dna.toCharArray();
+    for(int i = 0; i < chars.length; i++) {
+      chars[i] = getComplement(chars[i]);
+    }
+    
+    return new String(chars);
+  }
+  
+  private static char getComplement(char c) {
+    switch(c) {
+      case 'A': return 'T';
+      case 'T': return 'A';
+      case 'C': return 'G';
+      case 'G': return 'C';
+    }
+    return c;
+  }
+}
+
+// 자바 최신문법 너무 어려움....
