57b38304aacc1f713a80b5fc03aaf60a7b2457b1
pachipico <pachipico1@gmail.com>
2022-05-15 23:57:57 +0900

    Update 2022.05.15.js

--- 2022.05.15.js
+++ 2022.05.15.js
@@ -64,7 +64,35 @@ function justify(text, width) {
     let cnt = val.length - 1;
     let eachSpaces = Math.ceil(spaces / cnt);
     // 한 문장에 들어갈 단어들을 처리는 했으나 일정한 간격으로 띄워쓰기를 하는 방법을 못찾음..
+    let space = width - ("" + val.length);
+    for (var i = 0; i < spaces; i++) {
+      val[i % (val.length - 1)] += " ";
+    }
+    console.log(val);
   }, "");
 
   return res;
 }
+
+// 모범 답안
+var justify = function (str, len) {
+  var words = str.split(" ");
+  var output = [];
+  while (words.length) {
+    // Collect as many words as possible for the current line
+    var lineWords = [];
+    while (words.length > 0 && (lineWords + "," + words[0]).length <= len) lineWords.push(words.shift());
+
+    if (words.length) {
+      // No last line, so justify it
+      // Compute the holes between the words
+      var spaces = len - ("" + lineWords).length;
+      for (var i = 0; i < spaces; i++) lineWords[i % (lineWords.length - 1)] += " ";
+    }
+
+    // Build the line
+    output.push(lineWords.join(" "));
+  }
+
+  return output.join("\n");
+};
