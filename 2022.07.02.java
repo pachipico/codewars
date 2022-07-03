d0899a83e6a28f9e22e56f103143a829289b7504
pachipico <pachipico1@gmail.com>
2022-07-04 00:14:37 +0900

    Create 2022.07.03.java

--- 2022.07.02.java
+++ 2022.07.02.java
@@ -1,1 +1,21 @@
-d
+// Implement a function that accepts 3 integer values a, b, c. 
+// The function should return true if a triangle can be built with the sides of given length and false in any other case.
+
+// (In this case, all triangles must have surface greater than 0 to be accepted).
+// 내 답안
+import java.util.Arrays;
+
+class TriangleTester{
+  public static boolean isTriangle(int a, int b, int c) {
+		int[] arr = new int[] {a,b,c};
+		Arrays.sort(arr);
+		return arr[2] < arr[0] + arr[1];
+	}
+}
+
+// 모범 답안 
+class TriangleTester{
+  public static boolean isTriangle(int a, int b, int c){
+    return a + b > c && a + c > b && c + b > a;
+  }
+}
---
+++ 2022.07.03.java
@@ -0,0 +1,37 @@
+// Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language ) that receive a list of integers as input, and return the largest and lowest number in that list, respectively.
+
+// Examples (Input -> Output)
+// * [4,6,2,1,9,63,-134,566]         -> max = 566, min = -134
+// * [-52, 56, 30, 29, -54, 0, -110] -> min = -110, max = 56
+// * [42, 54, 65, 87, 0]             -> min = 0, max = 87
+// * [5]                             -> min = 5, max = 5
+
+// 내 답안
+import java.util.*;
+
+public class Kata {
+
+  public int min(int[] list) {
+		    Arrays.sort(list);
+		    return list[0];
+		  }
+		  
+		  public int max(int[] list) {
+			 Arrays.sort(list);
+		    return list[list.length-1];
+		  }
+}
+
+// 모범답안
+import java.util.Arrays;
+
+public class Kata {
+
+  public int min(int[] list) {
+    return Arrays.stream(list).min().getAsInt();
+  }
+  
+  public int max(int[] list) {
+    return Arrays.stream(list).max().getAsInt();
+  }
+}
