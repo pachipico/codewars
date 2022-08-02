d15d85487c44a83465e5cb0dc744620f93ac0634
pachipico <pachipico1@gmail.com>
2022-08-02 17:08:01 +0900

    Create 2022.08.02.java

---
+++ 2022.08.02.java
@@ -0,0 +1,23 @@
+// You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+
+// You can assume all values in the array are numbers.
+
+// 내 답안
+import java.util.Arrays;
+
+public class Kata
+{
+  public static boolean smallEnough(int[] a, int limit)
+  {
+    return Arrays.stream(a).max().getAsInt() <= limit;
+  }
+}
+
+// 모범 답안
+public class Kata
+{
+  public static boolean smallEnough(int[] a, int limit) {
+    return java.util.Arrays.stream(a).noneMatch(i -> i>limit);
+  }
+}
+
