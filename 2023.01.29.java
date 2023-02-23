de3211da0c0753ee0c44b9c8d2b9656e47bcc4bf
pachipico <pachipico1@gmail.com>
2023-02-23 23:51:00 +0900

    Create 2023.02.23.java

--- 2023.01.29.java
+++ 2023.01.29.java
@@ -1,3 +1,13 @@
-public class 2023.01.29 {
-  
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+class d {
+      public static int repeats(int [] arr){
+        //..
+        
+        String st = Arrays.stream(arr).sorted().boxed().map(String::valueOf).collect(Collectors.joining());
+        return Arrays.stream(st.split("(?<=(.))(?!\\1)")).filter(v -> v.length() == 1).mapToInt(Integer::parseInt).sum();
+    }
+
+
 }
