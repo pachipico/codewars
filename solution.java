3935f895258d877b6299a378db1e05c872952113
pachipico <pachipico1@gmail.com>
2023-04-05 17:21:18 +0900

    dd

--- solution.java
+++ solution.java
@@ -1,20 +1,76 @@
 import java.rmi.Remote;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.function.IntBinaryOperator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.LongStream;
 
 public class solution {
 
-//  "<prod><name>bicycle wheel</name><prx>150</prx><qty>11</qty></prod>\n\n"
-  public static String catalog(String s, String name) {
-    System.out.println(Arrays.stream(s.split("")).filter(v -> name.indexOf(v)>=0).distinct().collect(Collectors.joining()))
-    return "";
-  }
-
-
 
 }
+3*2*1
+// 3 2 1  => 1: 4, 2: 2, 3: 1
+
+// 1
+// 2
+// 3
+// 3 1
+// 3
+// 3 2 
+// 3 2 1
+
+
+// 4 3 2 1 => 1: 7, 2: 3, 3: 2, 4: 1
+// 1
+// 2
+// 3
+// 4
+// 4 1
+// 4
+// 4 2
+// 4
+// 4 3
+// 4 3 1
+// 4
+// 4 3 
+// 4 3 2
+// 4 3 2 1
+
+// abaa -> bba -> ca
+
+// 5 4 3 2 1 => 1: 11, 2: 7, 3: 4, 4: 2, 5: 1
+// 11 7 4 2 1
+// 1
+// 2
+// 3
+// 4
+// 5
+// 5 1
+// 5 
+// 5 2
+// 5 
+// 5 3
+// 5
+// 5 4
+// 5 4 1
+// 5 
+// 5 4
+// 5 4 2
+// 5 
+// 5 4
+// 5 4 3
+// 5 4 3 1
+// 5
+// 5 4
+// 5 4 3 
+// 5 4 3 2
+// 5 4 3 2 1
+
+// 3 7
+// 4 14
+// 5 25
+
